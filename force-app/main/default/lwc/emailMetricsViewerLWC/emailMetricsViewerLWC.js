196bb1a2ab1834946183df164ef51b8b4ef68b99
Emmanuel Martinez <jemmanuelmg@hotmail.com>
2020-04-08 17:01:34 -0500

    First version of the component finished. This commit is before adding the bootstrap styles and some other stetic enhancements

---
+++ force-app/main/default/lwc/emailMetricsViewerLWC/emailMetricsViewerLWC.js
@@ -0,0 +1,152 @@
+import { LightningElement, wire, track, api } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getInitialAccountList from '@salesforce/apex/EmailMetricsViewerController.getInitialAccountList';
+import queryAccountEmailMetrics from '@salesforce/apex/EmailMetricsViewerController.queryAccountEmailMetrics';
+
+
+export default class EmailMetricsViewerLWC extends LightningElement {
+
+    @track accountList = [];
+    @track value = '';
+    @track totalEmailsSent = 0;
+    @track totalOpens = 0;
+    @track openRate = 0;
+    @track totalBounces = 0;
+    @track bounceRate = 0;
+    @track totalCampaignMembers = 0;
+    @track totalUnsubscribes = 0;
+    @track unsubscribeRate = 0;
+    @track selectedAccountId = undefined;
+    
+
+    get accountComboboxOptions() {
+        return this.accountList;
+    }
+
+    connectedCallback() {
+        getInitialAccountList()
+            .then((data) => {
+
+                if (data.length > 0) {
+                    let comboValues = this.convertToComboboxValues(data); 
+                    this.accountList = comboValues;
+                }
+
+            })
+            .catch(error => {
+
+                console.log('>>> Error in getInitialAccountList()');
+                console.log(JSON.parse(JSON.stringify(error)));
+                this.showToastMessage('There has been an error. Please reload this page and try again', 'error');
+
+            })
+    }
+
+    getEmailMetricsForAccount(event) {
+
+        this.selectedAccountId = event.detail.value;
+
+        queryAccountEmailMetrics({accountId : this.selectedAccountId})
+            .then((data) => {
+
+                if (data.length > 0) {
+
+                    let metricList = JSON.parse(JSON.stringify(data));
+
+                    this.totalEmailsSent = metricList[0];
+                    this.totalOpens = metricList[1];
+                    this.totalBounces = metricList[2];
+                    this.totalCampaignMembers = metricList[3];
+                    this.totalUnsubscribes = metricList[4];
+
+                    if(this.totalEmailsSent != 0) {
+                        this.openRate = ((this.totalOpens / this.totalEmailsSent) * 100).toFixed(0);
+                        this.bounceRate = ((this.totalBounces / this.totalEmailsSent) * 100).toFixed(0);
+                    }else{
+                        this.openRate = 0;
+                        this.bounceRate = 0;
+                    }
+
+                    if(this.totalCampaignMembers != 0) {
+                        this.unsubscribeRate = ((this.totalUnsubscribes / this.totalCampaignMembers) * 100).toFixed(0);
+                    }else{
+                        this.unsubscribeRate = 0;
+                    }
+                }
+
+            })
+            .catch(error => {
+
+                console.log('>>> Error in getEmailMetricsForAccount()');
+                console.log(JSON.parse(JSON.stringify(error)));
+                this.showToastMessage('There has been an error. Please reload this page and try again', 'error');
+
+            })
+
+    }
+
+    convertToComboboxValues(data){
+
+        let currentData = [];
+
+            data.forEach((row) => {
+
+                let rowData = {};
+
+                rowData.label = row.Name;
+                rowData.value = row.Id;
+                
+                currentData.push(rowData);
+            });
+
+        return currentData;
+
+    }
+
+    redirectToReport(event) {
+
+        if(this.selectedAccountId != undefined){
+
+            let baseUrl = 'https://amg--emdev.lightning.force.com/lightning/r/Report/00O29000000a7iAEAQ/view?fv0=';
+            let finalUrl = baseUrl + this.selectedAccountId;
+
+            let tempLink = document.createElement('a');
+            tempLink.style.display = 'none';
+            document.body.appendChild(tempLink);
+            tempLink.setAttribute('href', finalUrl);
+            tempLink.setAttribute('target', '_blank');
+            tempLink.click();
+
+        }else{
+
+            this.showToastMessage('Please select an account to see its metrics report', 'info');
+
+        }
+        
+        
+        /*let baseUrl = 'https://amg--emdev.lightning.force.com/lightning/r/Report/00O29000000a7iAEAQ/view?fv0=';
+        let finalUrl = baseUrl + this.selectedAccountId;
+
+        let tempLink = document.createElement('a');
+        tempLink.style.display = 'none';
+        document.body.appendChild(tempLink);
+        tempLink.setAttribute('href', finalUrl);
+        a.setAttribute('target', '_blank');
+        tempLink.click();*/
+
+    }
+
+    showToastMessage(paramMessage, paramVariant){
+        
+        //Variant could be: success, warning, error
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: paramVariant,
+                message: paramMessage,
+                variant: paramVariant
+            })
+        );
+    }
+
+}
